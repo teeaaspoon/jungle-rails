e4524d5a11c61d1f0a1f1e163cbde521f23f0991
teeaaspoon <tommypoon36@gmail.com>
2018-07-19 02:13:38 +0000

    changed to validate if email has been already taken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
     
     def create
         @user = User.new(user_params)
-        p @user
         if @user.save
             session[:user_id] = @user.id
             redirect_to "/"
