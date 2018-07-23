45b26f018da734c34bd1c17c5728c80751670f54
teeaaspoon <tommypoon36@gmail.com>
2018-07-23 21:08:11 +0000

    fixed incorrect env variable name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.secret_token = ENV["SECRET_TOKEN"]
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.secret_token = ENV["secret_token"]
+  config.secret_key_base = ENV["secret_key_base"]
 
 end
